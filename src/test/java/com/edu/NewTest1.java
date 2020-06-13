89de9d217d22c6783c5a4c910aad23dab3123c23
lerndevops <lerndevops@gmail.com>
2020-06-13 18:48:52 +0530

    setets

---
+++ src/test/java/com/edu/NewTest1.java
@@ -0,0 +1,53 @@
+package com.edu;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeMethod;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+
+public class NewTest1 {
+	
+  WebDriver driver;
+ 
+  @BeforeMethod
+  public void beforeMethod() {
+	  
+	    System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
+	    driver = new ChromeDriver();
+		driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+		driver.get("http://35.232.128.200:32768/sampleapp/");
+  }
+  
+  @Test
+  public void addContact() {
+	  
+	 	driver.findElement(By.xpath("//div[@class = 'v-button v-widget']")).click();
+		driver.findElement(By.id("gwt-uid-5")).sendKeys("Naresh");
+		driver.findElement(By.id("gwt-uid-7")).sendKeys("T");
+		driver.findElement(By.id("gwt-uid-9")).sendKeys("805983095");
+		driver.findElement(By.id("gwt-uid-11")).sendKeys("test@gmail.com");
+		driver.findElement(By.xpath("//div[@class = 'v-button v-widget primary v-button-primary']")).click();
+			
+	}
+  
+  @Test
+  public void verifytitle() {
+	  
+        String a = driver.getTitle();
+        System.out.println(a);
+        Assert.assertEquals(a, "sampleapp");
+	    System.out.println("Test Successful."); 
+  }
+  
+  @AfterMethod
+  public void afterMethod() {  
+	  driver.close();
+  }
+
+}
---
+++ src/test/java/com/edu/NewTest2.java
@@ -0,0 +1,49 @@
+package com.edu;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeMethod;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+
+public class NewTest2 {
+	WebDriver driver;
+
+  @BeforeMethod
+  public void launch() {
+	  
+	    System.setProperty("webdriver.chrome.driver", "C:\\Users\\Nareshwar\\Downloads\\chromedriver.exe");
+	    driver = new ChromeDriver();
+	    driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+	    driver.get("https://facebook.com");
+
+	  }
+  
+  @Test
+  public void verifyurl() {
+	  
+	    String x = driver.getCurrentUrl();
+	    System.out.println(x);
+	    Assert.assertEquals(x, "https://www.facebook.com/");
+  }
+  
+  @Test
+  public void verifytitle() {
+	  
+        String a = driver.getTitle();
+        System.out.println(a);
+        Assert.assertEquals(a, "Facebook");
+	    //System.out.println("Test Successful.");    
+  }
+  
+  @AfterMethod
+  public void close() {
+	  
+	  driver.close();
+  }
+
+}
