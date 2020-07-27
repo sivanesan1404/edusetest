71e936bc50833c3558ef0c8ce86d9bc74c25f9bb
sivanesan1404 <67639947+sivanesan1404@users.noreply.github.com>
2020-07-28 01:22:47 +0530

    Update NewTest1.java

--- src/test/java/com/edu/NewTest1.java
+++ src/test/java/com/edu/NewTest1.java
@@ -18,7 +18,7 @@ public class NewTest1 {
   @BeforeMethod
   public void beforeMethod() {
 	  
-	    System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
+	    System.setProperty("webdriver.chrome.driver", "chromedriver");
 	    driver = new ChromeDriver();
 		driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
 		driver.get("http://18.144.147.32:32768");
